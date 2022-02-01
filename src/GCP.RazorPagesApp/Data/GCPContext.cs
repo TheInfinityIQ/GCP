2eb0a109066c8d647863ebad8b23bfa5ceb2b8a9
Dawson K <46154365+TheInfinityIQ@users.noreply.github.com>
2022-02-01 13:44:37 -0600

    Set Up Database
    - Added GCPContext (DbContext)
    - Added InitialMigration
    - Updated dev appsettings.json with connection string
    - Created Entities folder with Game table

---
+++ src/GCP.RazorPagesApp/Data/GCPContext.cs
@@ -0,0 +1,16 @@
+﻿using GCP.RazorPagesApp.Data.Entities;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace GCP.RazorPagesApp.Data;
+
+public class GCPContext	: DbContext
+{
+	public GCPContext(DbContextOptions<GCPContext> options) : base(options)
+	{
+
+	}
+
+	public DbSet<Game> Game { get; set; }
+
+}
