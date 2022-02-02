319ba6e83440de230dae397a46c06afea8615cf0
Everett Grassler <45568848+Dragwar@users.noreply.github.com>
2022-02-01 23:22:00 -0600

    added base module types for minimal APIs

---
+++ src/GCP.RazorPagesApp/Api/Module/IModule.cs
@@ -0,0 +1,13 @@
+﻿namespace GCP.RazorPagesApp.Api.Module;
+public interface IModule
+{
+	void AddServices(IServiceCollection services);
+	void MapEndpoints(IEndpointRouteBuilder app);
+}
+
+public abstract class BaseModule : IModule
+{
+	public virtual string RoutePrefix { get; } = "/api";
+	public abstract void AddServices(IServiceCollection services);
+	public abstract void MapEndpoints(IEndpointRouteBuilder app);
+}
