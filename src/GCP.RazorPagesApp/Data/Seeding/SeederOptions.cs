96667e743ec1668905f4ce6efb196a09ace18e40
Everett Grassler <45568848+Dragwar@users.noreply.github.com>
2022-02-02 18:46:49 -0600

    added seeder options + updated appsettings

---
+++ src/GCP.RazorPagesApp/Data/Seeding/SeederOptions.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace GCP.RazorPagesApp.Data.Seeding;
+
+public record SeederOptions
+{
+	public const string KEY = nameof(SeederOptions);
+
+	[Required]
+	public HashSet<DatabaseMigrationOption> DatabaseMigrationOptions { get; init; } = new HashSet<DatabaseMigrationOption>();
+
+	[Required]
+	public string SharedTestPassword { get; init; } = default!;
+}
