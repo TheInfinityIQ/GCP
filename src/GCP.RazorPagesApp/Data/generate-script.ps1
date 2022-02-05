42cd1b45f7cc5a07bdcbffa47f7189172381f0c8
Everett Grassler <45568848+Dragwar@users.noreply.github.com>
2022-02-05 00:29:48 -0600

    create migration script during docker build
    (for future use inside docker image/artifact)

---
+++ src/GCP.RazorPagesApp/Data/generate-script.ps1
@@ -0,0 +1,12 @@
+﻿
+$alreadyInstalled = dotnet tool list -g | Select-String -Pattern "dotnet-ef" -Quiet
+
+if (-not $alreadyInstalled) {
+    echo "Installing dotnet-ef..."
+    dotnet tool install -g dotnet-ef
+    echo "Installed dotnet-ef."
+}
+
+echo "Generating database scripts for GCP.RazorPagesApp..."
+dotnet ef migrations script -i -o "./Data/Scripts/migrations.sql" --no-build
+echo "Scripts generated successfully."
