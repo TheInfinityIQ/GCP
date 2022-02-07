f046f0529eab54ef9b250106fa161a9fc3c23a2a
Everett Grassler <45568848+Dragwar@users.noreply.github.com>
2022-02-06 21:47:54 -0600

    re-implement post/put game APIs

--- src/GCP.RazorPagesApp/Api/Module/GameModule/DTOs.cs
+++ src/GCP.RazorPagesApp/Api/Module/GameModule/DTOs.cs
@@ -2,10 +2,13 @@
 
 namespace GCP.RazorPagesApp.Api.Module.GameModule;
 
-public record Link(Uri Url, LinkType Type, string? Label);
-public record Endorsement(string User, double Weight);
+public record LinkDTO(int Id, Uri Url, LinkType Type, string? Label);
+public record EndorsementDTO(int Id, int UserId, string User, double Weight);
+public record NameRecordDTO(int Id, string Name);
 
-public record Publisher(string Name, IEnumerable<string> Platforms);
+public record PublisherDTO(int Id, string Name, IEnumerable<NameRecordDTO> Platforms)
+	: NameRecordDTO(Id, Name);
+
 
 public record GameListItemDTO(
 	int Id,
@@ -13,12 +16,11 @@ public record GameListItemDTO(
 	DateOnly? ReleaseDate,
 	int MaxPlayers,
 	bool SupportsOnlinePlay,
-	IEnumerable<string> Genres,
-	IEnumerable<string> Platforms,
-	IEnumerable<string> Retailers,
-	IEnumerable<Publisher> Publishers,
-	IEnumerable<Link> Links,
-	IEnumerable<Endorsement> Endorsements
+	IEnumerable<NameRecordDTO> Genres,
+	IEnumerable<NameRecordDTO> Retailers,
+	IEnumerable<PublisherDTO> Publishers,
+	IEnumerable<LinkDTO> Links,
+	IEnumerable<EndorsementDTO> Endorsements
 )
 {
 	public GameListItemDTO(Game game)
@@ -28,17 +30,40 @@ public record GameListItemDTO(
 			game.ReleaseDate,
 			game.MaxPlayers,
 			game.SupportsOnlinePlay,
-			game.Genres.Select(g => g.Name),
-			game.GamePublisherPlatforms.Select(gpp => gpp.Platform.Name),
-			game.Retailers.Select(r => r.Name),
-			game.GamePublishers.Select(gp => new Publisher(gp.Publisher.Name, gp.Platforms.Select(p => p.Name))),
-			game.GameLinks.Select(gl => new Link(gl.Link, gl.Type, gl.Label)),
-			game.Endorsements.Select(e => new Endorsement(e.User.DisplayName ?? e.User.UserName, e.Weight))
+			game.Genres.Select(g => new NameRecordDTO(g.Id, g.Name)),
+			game.Retailers.Select(r => new NameRecordDTO(r.Id, r.Name)),
+			game.GamePublishers.Select(gp => new PublisherDTO(gp.PublisherId, gp.Publisher.Name, gp.Platforms.Select(p => new NameRecordDTO(p.Id, p.Name)))),
+			game.GameLinks.Select(gl => new LinkDTO(gl.Id, gl.Link, gl.Type, gl.Label)),
+			game.Endorsements.Select(e => new EndorsementDTO(e.Id, e.UserId, e.User.DisplayName ?? e.User.UserName, e.Weight))
 		)
 	{
 	}
 }
 
-public record CreateGameDTO(string Link, int Score, string User)
+public record CreateGameDTO(
+	string Title,
+	DateOnly? ReleaseDate,
+	int MaxPlayers,
+	bool SupportsOnlinePlay,
+	EndorsementDTO? Endorsement,
+	IEnumerable<NameRecordDTO> Genres,
+	IEnumerable<NameRecordDTO> Retailers,
+	IEnumerable<PublisherDTO> Publishers,
+	IEnumerable<LinkDTO> Links
+)
+{
+}
+
+public record UpdateGameDTO(
+	string Title,
+	DateOnly? ReleaseDate,
+	int MaxPlayers,
+	bool SupportsOnlinePlay,
+	IEnumerable<EndorsementDTO> Endorsements,
+	IEnumerable<NameRecordDTO> Genres,
+	IEnumerable<NameRecordDTO> Retailers,
+	IEnumerable<PublisherDTO> Publishers,
+	IEnumerable<LinkDTO> Links
+)
 {
 }
--- src/GCP.RazorPagesApp/Api/Module/GameModule/GameModule.cs
+++ src/GCP.RazorPagesApp/Api/Module/GameModule/GameModule.cs
@@ -1,4 +1,6 @@
-﻿using GCP.RazorPagesApp.Data;
+﻿
+using GCP.RazorPagesApp.Data;
+using GCP.RazorPagesApp.Data.Entities;
 
 using Microsoft.EntityFrameworkCore;
 
@@ -67,97 +69,436 @@ public class GameModule : BaseModule
 			.Produces<GameListItemDTO>(200)
 			.Produces(404);
 
-		//var postGame = app.MapPost(BaseUrl, async (CreateGameDTO inputDTO, GCPContext context, CancellationToken cancellationToken) =>
-		//{
-		//	var (link, score, user) = inputDTO;
+		var postGame = app.MapPost(BaseUrl, async (CreateGameDTO inputDTO, GCPContext context, CancellationToken cancellationToken) =>
+		{
+			var newGame = new Game();
+			var errors = new Dictionary<string, string[]>();
 
-		//	var errors = new Dictionary<string, string[]>();
-		//	if (!Uri.TryCreate(link, UriKind.Absolute, out _))
-		//	{
-		//		errors.Add(nameof(link), new[] { "must be a valid absolute link." });
-		//	}
+			foreach (var link in inputDTO.Links ?? Array.Empty<LinkDTO>())
+			{
+				//if (!Uri.TryCreate(link.Url, UriKind.Absolute, out _))
+				if (string.IsNullOrWhiteSpace(link.Url?.OriginalString))
+				{
+					errors.Add(nameof(link), new[] { "must be a valid absolute link." });
+					continue;
+				}
 
-		//	if (score is < 0)
-		//	{
-		//		errors.Add(nameof(score), new[] { "can't be less than zero." });
-		//	}
+				newGame.GameLinks.Add(new() { Link = link.Url, Label = link.Label, Type = link.Type });
+			}
 
-		//	if (string.IsNullOrWhiteSpace(user))
-		//	{
-		//		errors.Add(nameof(user), new[] { "cannot be null or whitespace." });
-		//	}
+			if (inputDTO.Endorsement is not null)
+			{
+				var users = await context.Users.ToListAsync(cancellationToken);
+				if (inputDTO.Endorsement.Weight is < 0)
+				{
+					errors.Add(nameof(inputDTO.Endorsement.Weight), new[] { "can't be less than zero." });
+				}
 
-		//	if (errors.Any())
-		//	{
-		//		return Results.BadRequest(new HttpValidationProblemDetails(errors));
-		//	}
+				var user = users.SingleOrDefault(u => u.Id == inputDTO.Endorsement.UserId);
+				if (user is null)
+				{
+					errors.Add(nameof(inputDTO.Endorsement.UserId), new[] { "user not found." });
+				}
+				else
+				{
+					newGame.Endorsements.Add(new() { Weight = inputDTO.Endorsement.Weight, UserId = user.Id });
+				}
+			}
 
-		//	var game = new Data.Entities.Game { Score = score, GameLink = link, User = user };
 
-		//	await context.Game.AddAsync(game, cancellationToken);
-		//	await context.SaveChangesAsync(cancellationToken);
 
-		//	var dto = new GameListItemDTO(game);
+			var publishers = await context.Publisher.ToListAsync(cancellationToken);
+			var platforms = await context.Platform.ToListAsync(cancellationToken);
+			foreach (var publisherDTO in inputDTO.Publishers ?? Array.Empty<PublisherDTO>())
+			{
+				var publisher = publishers.SingleOrDefault(p => p.Id == publisherDTO.Id);
+				if (publisherDTO.Id is 0)
+				{
+					if (string.IsNullOrWhiteSpace(publisherDTO.Name))
+					{
+						errors.Add(nameof(inputDTO.Publishers), new[] { "publisher name cannot be null or whitespace." });
+						continue;
+					}
+					publisher = new()
+					{
+						Name = publisherDTO.Name,
+						NormalizedName = publisherDTO.Name.ToUpperInvariant(),
+					};
 
-		//	return Results.Created(BaseUrl + $"/{game.Id}", dto);
-		//});
-		//postGame
-		//	.AllowAnonymous()
-		//	.WithTags(nameof(GameModule))
-		//	.Accepts<CreateGameDTO>(isOptional: false, "application/json")
-		//	.Produces<GameListItemDTO>(200)
-		//	.Produces<HttpValidationProblemDetails>(400)
-		//	.Produces(404);
+					if (publishers.Any(p => p.NormalizedName == publisher.NormalizedName))
+					{
+						errors.Add(nameof(inputDTO.Publishers), new[] { "publisher name is already taken." });
+						continue;
+					}
+				}
+				else if (publisher is null)
+				{
+					errors.Add(nameof(inputDTO.Publishers), new[] { $"publisher '{publisherDTO.Id}' not found." });
+					continue;
+				}
 
-		//var putGame = app.MapPut($"{BaseUrl}/{{id:int}}", async (int id, CreateGameDTO inputDTO, GCPContext context, CancellationToken cancellationToken) =>
-		//{
-		//	var game = await context.Game.Where(x => x.Id == id).SingleOrDefaultAsync(cancellationToken);
-		//	if (game is null)
-		//	{
-		//		return Results.NotFound();
-		//	}
+				var publisherPlatforms = new List<Platform>();
+				foreach (var platformDTO in publisherDTO.Platforms ?? Array.Empty<NameRecordDTO>())
+				{
+					var platform = platforms.SingleOrDefault(p => p.Id == platformDTO.Id);
+					if (platformDTO.Id is 0)
+					{
+						if (string.IsNullOrWhiteSpace(platformDTO.Name))
+						{
+							errors.Add(nameof(inputDTO.Publishers), new[] { "platform name cannot be null or whitespace." });
+							continue;
+						}
+						platform = new()
+						{
+							Name = platformDTO.Name,
+							NormalizedName = platformDTO.Name.ToUpperInvariant(),
+						};
 
-		//	var (link, score, user) = inputDTO;
+						if (platforms.Any(p => p.NormalizedName == platform.NormalizedName))
+						{
+							errors.Add(nameof(inputDTO.Publishers), new[] { "platform name is already taken." });
+							continue;
+						}
+					}
+					else if (platform is null)
+					{
+						errors.Add(nameof(inputDTO.Publishers), new[] { $"publisher's '{publisher.Id}' platform '{platformDTO.Id}' not found." });
+						continue;
+					}
+					publisherPlatforms.Add(platform);
+				}
 
-		//	var errors = new Dictionary<string, string[]>();
-		//	if (!Uri.TryCreate(link, UriKind.Absolute, out _))
-		//	{
-		//		errors.Add(nameof(link), new[] { "must be a valid absolute link." });
-		//	}
+				newGame.GamePublishers.Add(new()
+				{
+					Publisher = publisher,
+					Platforms = publisherPlatforms,
+				});
+			}
 
-		//	if (score is < 0)
-		//	{
-		//		errors.Add(nameof(score), new[] { "can't be less than zero." });
-		//	}
 
-		//	if (string.IsNullOrWhiteSpace(user))
-		//	{
-		//		errors.Add(nameof(user), new[] { "cannot be null or whitespace." });
-		//	}
+			var genres = await context.Genre.ToListAsync(cancellationToken);
+			foreach (var genreDTO in inputDTO.Genres ?? Array.Empty<NameRecordDTO>())
+			{
+				var genre = genres.SingleOrDefault(g => g.Id == genreDTO.Id);
+				if (genreDTO.Id is 0)
+				{
+					if (string.IsNullOrWhiteSpace(genreDTO.Name))
+					{
+						errors.Add(nameof(inputDTO.Genres), new[] { "genre name cannot be null or whitespace." });
+						continue;
+					}
+					genre = new()
+					{
+						Name = genreDTO.Name,
+						NormalizedName = genreDTO.Name.ToUpperInvariant(),
+					};
 
-		//	if (errors.Any())
-		//	{
-		//		return Results.BadRequest(new HttpValidationProblemDetails(errors));
-		//	}
+					if (genres.Any(p => p.NormalizedName == genre.NormalizedName))
+					{
+						errors.Add(nameof(inputDTO.Genres), new[] { "genre name is already taken." });
+						continue;
+					}
+				}
+				else if (genre is null)
+				{
+					errors.Add(nameof(inputDTO.Genres), new[] { $"genre '{genreDTO.Id}' not found." });
+					continue;
+				}
+				newGame.Genres.Add(genre);
+			}
 
-		//	game.GameLink = link;
-		//	game.Score = score;
-		//	game.User = user;
+			var retailers = await context.Retailer.ToListAsync(cancellationToken);
+			foreach (var retailerDTO in inputDTO.Retailers ?? Array.Empty<NameRecordDTO>())
+			{
+				var retailer = retailers.SingleOrDefault(g => g.Id == retailerDTO.Id);
+				if (retailerDTO.Id is 0)
+				{
+					if (string.IsNullOrWhiteSpace(retailerDTO.Name))
+					{
+						errors.Add(nameof(inputDTO.Retailers), new[] { "retailer name cannot be null or whitespace." });
+						continue;
+					}
+					retailer = new()
+					{
+						Name = retailerDTO.Name,
+						NormalizedName = retailerDTO.Name.ToUpperInvariant(),
+					};
 
-		//	context.Game.Update(game);
-		//	await context.SaveChangesAsync(cancellationToken);
+					if (retailers.Any(p => p.NormalizedName == retailer.NormalizedName))
+					{
+						errors.Add(nameof(inputDTO.Retailers), new[] { "retailer name is already taken." });
+						continue;
+					}
+				}
+				else if (retailer is null)
+				{
+					errors.Add(nameof(inputDTO.Publishers), new[] { $"retailer '{retailerDTO.Id}' not found." });
+					continue;
+				}
+				newGame.Retailers.Add(retailer);
+			}
 
-		//	var dto = new GameListItemDTO(game);
 
-		//	return Results.Ok(dto);
-		//});
-		//putGame
-		//	.AllowAnonymous()
-		//	.WithTags(nameof(GameModule))
-		//	.Accepts<CreateGameDTO>(isOptional: false, "application/json")
-		//	.Produces<GameListItemDTO>(200)
-		//	.Produces<HttpValidationProblemDetails>(400)
-		//	.Produces(404);
+			if (string.IsNullOrWhiteSpace(inputDTO.Title))
+			{
+				errors.Add(nameof(inputDTO.Title), new[] { "title cannot be null or whitespace." });
+			}
+			else
+			{
+				newGame.Title = inputDTO.Title;
+				newGame.NormalizedTitle = inputDTO.Title.ToUpperInvariant();
+			}
+
+			if (await context.Game.AnyAsync(g => g.NormalizedTitle == newGame.NormalizedTitle, cancellationToken) is true)
+			{
+				errors.Add(nameof(inputDTO.Title), new[] { "title is already taken." });
+			}
+
+			newGame.ReleaseDate = inputDTO.ReleaseDate;
+			newGame.SupportsOnlinePlay = inputDTO.SupportsOnlinePlay;
+			newGame.MaxPlayers = inputDTO.MaxPlayers;
+
+			if (errors.Any())
+			{
+				return Results.BadRequest(new HttpValidationProblemDetails(errors));
+			}
+
+			await context.Game.AddAsync(newGame, cancellationToken);
+			await context.SaveChangesAsync(cancellationToken);
+
+			var dto = new GameListItemDTO(newGame);
+
+			return Results.Created(BaseUrl + $"/{newGame.Id}", dto);
+		});
+		postGame
+			.AllowAnonymous()
+			.WithTags(nameof(GameModule))
+			.Accepts<CreateGameDTO>(isOptional: false, "application/json")
+			.Produces<GameListItemDTO>(200)
+			.Produces<HttpValidationProblemDetails>(400)
+			.Produces(404);
+
+		var putGame = app.MapPut($"{BaseUrl}/{{id:int}}", async (int id, UpdateGameDTO inputDTO, GCPContext context, CancellationToken cancellationToken) =>
+		{
+			var errors = new Dictionary<string, string[]>();
+			var newGame = await context.Game.SingleOrDefaultAsync(g => g.Id == id, cancellationToken);
+			if (newGame is null)
+			{
+				errors.Add(nameof(id), new[] { $"game not found by id '{id}'" });
+				return Results.NotFound(new HttpValidationProblemDetails(errors));
+			}
+			newGame.Genres.Clear();
+			newGame.Retailers.Clear();
+			newGame.Publishers.Clear();
+			newGame.Endorsements.Clear();
+			newGame.GameLinks.Clear();
+			newGame.GameGenres.Clear();
+			newGame.GameRetailers.Clear();
+			newGame.GamePublishers.Clear();
+			newGame.GamePublisherPlatforms.Clear();
+
+			foreach (var link in inputDTO.Links ?? Array.Empty<LinkDTO>())
+			{
+				//if (!Uri.TryCreate(link.Url, UriKind.Absolute, out _))
+				if (string.IsNullOrWhiteSpace(link.Url?.OriginalString))
+				{
+					errors.Add(nameof(link), new[] { "must be a valid absolute link." });
+					continue;
+				}
+
+				newGame.GameLinks.Add(new() { Link = link.Url, Label = link.Label, Type = link.Type });
+			}
+
+			var users = await context.Users.ToListAsync(cancellationToken);
+			foreach (var endorsement in inputDTO.Endorsements ?? Array.Empty<EndorsementDTO>())
+			{
+				if (endorsement.Weight is < 0)
+				{
+					errors.Add(nameof(endorsement.Weight), new[] { "can't be less than zero." });
+				}
+
+				var user = users.SingleOrDefault(u => u.Id == endorsement.UserId);
+				if (user is null)
+				{
+					errors.Add(nameof(endorsement.UserId), new[] { "user not found." });
+				}
+				else
+				{
+					newGame.Endorsements.Add(new() { Weight = endorsement.Weight, UserId = user.Id });
+				}
+			}
+
+
+			var publishers = await context.Publisher.ToListAsync(cancellationToken);
+			var platforms = await context.Platform.ToListAsync(cancellationToken);
+			foreach (var publisherDTO in inputDTO.Publishers ?? Array.Empty<PublisherDTO>())
+			{
+				var publisher = publishers.SingleOrDefault(p => p.Id == publisherDTO.Id);
+				if (publisherDTO.Id is 0)
+				{
+					if (string.IsNullOrWhiteSpace(publisherDTO.Name))
+					{
+						errors.Add(nameof(inputDTO.Publishers), new[] { "publisher name cannot be null or whitespace." });
+						continue;
+					}
+					publisher = new()
+					{
+						Name = publisherDTO.Name,
+						NormalizedName = publisherDTO.Name.ToUpperInvariant(),
+					};
+
+					if (publishers.Any(p => p.NormalizedName == publisher.NormalizedName))
+					{
+						errors.Add(nameof(inputDTO.Publishers), new[] { "publisher name is already taken." });
+						continue;
+					}
+				}
+				else if (publisher is null)
+				{
+					errors.Add(nameof(inputDTO.Publishers), new[] { $"publisher '{publisherDTO.Id}' not found." });
+					continue;
+				}
+
+				var publisherPlatforms = new List<Platform>();
+				foreach (var platformDTO in publisherDTO.Platforms ?? Array.Empty<NameRecordDTO>())
+				{
+					var platform = platforms.SingleOrDefault(p => p.Id == platformDTO.Id);
+					if (platformDTO.Id is 0)
+					{
+						if (string.IsNullOrWhiteSpace(platformDTO.Name))
+						{
+							errors.Add(nameof(inputDTO.Publishers), new[] { "platform name cannot be null or whitespace." });
+							continue;
+						}
+						platform = new()
+						{
+							Name = platformDTO.Name,
+							NormalizedName = platformDTO.Name.ToUpperInvariant(),
+						};
+
+						if (platforms.Any(p => p.NormalizedName == platform.NormalizedName))
+						{
+							errors.Add(nameof(inputDTO.Publishers), new[] { "platform name is already taken." });
+							continue;
+						}
+					}
+					else if (platform is null)
+					{
+						errors.Add(nameof(inputDTO.Publishers), new[] { $"publisher's '{publisher.Id}' platform '{platformDTO.Id}' not found." });
+						continue;
+					}
+					publisherPlatforms.Add(platform);
+				}
+
+				newGame.GamePublishers.Add(new()
+				{
+					Publisher = publisher,
+					Platforms = publisherPlatforms,
+				});
+			}
+
+
+			var genres = await context.Genre.ToListAsync(cancellationToken);
+			foreach (var genreDTO in inputDTO.Genres ?? Array.Empty<NameRecordDTO>())
+			{
+				var genre = genres.SingleOrDefault(g => g.Id == genreDTO.Id);
+				if (genreDTO.Id is 0)
+				{
+					if (string.IsNullOrWhiteSpace(genreDTO.Name))
+					{
+						errors.Add(nameof(inputDTO.Genres), new[] { "genre name cannot be null or whitespace." });
+						continue;
+					}
+					genre = new()
+					{
+						Name = genreDTO.Name,
+						NormalizedName = genreDTO.Name.ToUpperInvariant(),
+					};
+
+					if (genres.Any(p => p.NormalizedName == genre.NormalizedName))
+					{
+						errors.Add(nameof(inputDTO.Genres), new[] { "genre name is already taken." });
+						continue;
+					}
+				}
+				else if (genre is null)
+				{
+					errors.Add(nameof(inputDTO.Genres), new[] { $"genre '{genreDTO.Id}' not found." });
+					continue;
+				}
+				newGame.Genres.Add(genre);
+			}
+
+			var retailers = await context.Retailer.ToListAsync(cancellationToken);
+			foreach (var retailerDTO in inputDTO.Retailers ?? Array.Empty<NameRecordDTO>())
+			{
+				var retailer = retailers.SingleOrDefault(g => g.Id == retailerDTO.Id);
+				if (retailerDTO.Id is 0)
+				{
+					if (string.IsNullOrWhiteSpace(retailerDTO.Name))
+					{
+						errors.Add(nameof(inputDTO.Retailers), new[] { "retailer name cannot be null or whitespace." });
+						continue;
+					}
+					retailer = new()
+					{
+						Name = retailerDTO.Name,
+						NormalizedName = retailerDTO.Name.ToUpperInvariant(),
+					};
+
+					if (retailers.Any(p => p.NormalizedName == retailer.NormalizedName))
+					{
+						errors.Add(nameof(inputDTO.Retailers), new[] { "retailer name is already taken." });
+						continue;
+					}
+				}
+				else if (retailer is null)
+				{
+					errors.Add(nameof(inputDTO.Publishers), new[] { $"retailer '{retailerDTO.Id}' not found." });
+					continue;
+				}
+				newGame.Retailers.Add(retailer);
+			}
+
+
+			if (string.IsNullOrWhiteSpace(inputDTO.Title))
+			{
+				errors.Add(nameof(inputDTO.Title), new[] { "title cannot be null or whitespace." });
+			}
+			else
+			{
+				newGame.Title = inputDTO.Title;
+				newGame.NormalizedTitle = inputDTO.Title.ToUpperInvariant();
+			}
+
+			if (await context.Game.AnyAsync(g => g.NormalizedTitle == newGame.NormalizedTitle, cancellationToken) is true)
+			{
+				errors.Add(nameof(inputDTO.Title), new[] { "title is already taken." });
+			}
+
+			newGame.ReleaseDate = inputDTO.ReleaseDate;
+			newGame.SupportsOnlinePlay = inputDTO.SupportsOnlinePlay;
+			newGame.MaxPlayers = inputDTO.MaxPlayers;
+
+			if (errors.Any())
+			{
+				return Results.BadRequest(new HttpValidationProblemDetails(errors));
+			}
+
+			await context.Game.AddAsync(newGame, cancellationToken);
+			await context.SaveChangesAsync(cancellationToken);
+
+			var dto = new GameListItemDTO(newGame);
+
+			return Results.Created(BaseUrl + $"/{newGame.Id}", dto);
+		});
+		putGame
+			.AllowAnonymous()
+			.WithTags(nameof(GameModule))
+			.Accepts<CreateGameDTO>(isOptional: false, "application/json")
+			.Produces<GameListItemDTO>(200)
+			.Produces<HttpValidationProblemDetails>(400)
+			.Produces(404);
 
 		var deleteGame = app.MapDelete($"{BaseUrl}/{{id:int}}", async (int id, GCPContext context, CancellationToken cancellationToken) =>
 		{
