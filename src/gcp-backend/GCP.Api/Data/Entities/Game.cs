1b54ae0022f517e2064b85b5db91d3061394de36
Everett Grassler <45568848+Dragwar@users.noreply.github.com>
2022-04-03 05:20:52 -0500

    added early game entity

---
+++ src/gcp-backend/GCP.Api/Data/Entities/Game.cs
@@ -0,0 +1,32 @@
+﻿namespace GCP.Api.Data.Entities;
+public class Game
+{
+	public int Id { get; init; }
+	public string Name { get; set; } = default!;
+	public string NormalizedName { get; set; } = default!;
+	public GameMetadata Metadata { get; init; } = new GameMetadata();
+	public string? SteamAppId { get; set; }
+	public DateOnly? ReleaseDate { get; set; }
+}
+
+
+public class GameMetadata
+{
+	public HashSet<string> Aliases { get; init; } = new HashSet<string>();
+	public HashSet<string> Genres { get; init; } = new HashSet<string>();
+	public HashSet<string> Platforms { get; init; } = new HashSet<string>();
+	public HashSet<string> Launchers { get; init; } = new HashSet<string>();
+
+
+	public string? IconUrl { get; set; }
+	public GameImageUrlSet Card { get; init; } = new GameImageUrlSet();
+	public GameImageUrlSet Background { get; init; } = new GameImageUrlSet();
+}
+
+
+public class GameImageUrlSet
+{
+	public string? Small { get; set; }
+	public string? Medium { get; set; }
+	public string? Large { get; set; }
+}
