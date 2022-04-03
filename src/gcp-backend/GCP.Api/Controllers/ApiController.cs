66e1a89dcf1a58494ce51c9c3e775a8441bf3859
Everett Grassler <45568848+Dragwar@users.noreply.github.com>
2022-04-03 01:28:37 -0500

    added dummy secret test controller

---
+++ src/gcp-backend/GCP.Api/Controllers/ApiController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GCP.Api.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+[Produces("application/json")]
+public abstract class ApiController : Controller
+{
+}
---
+++ src/gcp-backend/GCP.Api/Controllers/SecretController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GCP.Api.Controllers;
+
+public class SecretController : ApiController
+{
+	[HttpGet]
+	public ActionResult<SecretDTO> GetSecret() => Ok(new SecretDTO("My very secret value."));
+
+	[HttpGet("public")]
+	[AllowAnonymous]
+	public ActionResult<SecretDTO> GetPublicSecret() => Ok(new SecretDTO("My not well kept secret value."));
+}
+
+
+public record SecretDTO(string Value);
