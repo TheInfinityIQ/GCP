fa5c8d98a0fac76823a82037e59e2a7a50bbf58c
Dawson K <46154365+TheInfinityIQ@users.noreply.github.com>
2022-04-06 14:54:22 -0500

    Search bar in place. Added content for pill menus

--- src/gcp-ui/src/App.vue
+++ src/gcp-ui/src/App.vue
@@ -1,161 +1,190 @@
 <script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
 import { ref } from "vue";
-const msg = ref("Hello");
-const count = ref(0);
+const stuff = ref([1, 2]);
 </script>
 
 <template>
-    <!-- Header start -->
-    <header class="inline">
-        <img src="./assets/Logo.svg" alt="Logo" />
-        <h1>Game Chooser Program</h1>
-    </header>
-    <!-- Header end -->
+  <!-- Header start -->
+  <header class="inline">
+      <img src="./assets/Logo.svg" alt="Logo" />
+      <h1>Game Chooser Program</h1>
+  </header>
+  <!-- Header end -->
 
-    <!-- Content in body should adjust image so that there is no whitespace -->
-    <section>
-        <img alt="Vue logo" src="./assets/logo.png" />
-        <h1>{{ msg }}</h1>
+  <!-- Content in body should adjust image so that there is no whitespace -->
+  <section class="search-menu">
+    <div class="frosted-background">
+      <section class="search-field">
+        <p>Search for games:</p>
+        <input type="text" id="search">
+      </section>
+      <div class="pill-menu" id="sort"></div>
+      <div class="pill-menu" id="platform"></div>
+        <div class="pill-menu-content pill-menu-sort">
+          <ul>
+            <li>Newest</li>
+            <li>Oldest</li>
+            <li>Most People</li>
+            <li>Most Active</li>
+          </ul>
+        </div>
+        <div class="pill-menu-content pill-menu-platform">
+          <ul>
+            <li>Steam</li>
+            <li>Xbox</li>
+            <li>Playstation</li>
+            <li>Web</li>
+          </ul>
+        </div>
+    </div>
+  </section>
+  <!-- Body end -->
 
-        <p>
-            Recommended IDE setup:
-            <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-            +
-            <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-        </p>
-
-        <p>See <code>README.md</code> for more information.</p>
-
-        <p>
-            <a href="https://vitejs.dev/guide/features.html" target="_blank"> Vite Docs </a>
-            |
-            <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-        </p>
-
-        <button type="button" @click="count++">count is: {{ count }}</button>
-        <p>
-            Edit
-            <code>components/HelloWorld.vue</code> to test hot module replacement.
-        </p>
-    </section>
-    <section>
-        <img alt="Vue logo" src="./assets/logo.png" />
-        <h1>{{ msg }}</h1>
-
-        <p>
-            Recommended IDE setup:
-            <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-            +
-            <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-        </p>
-
-        <p>See <code>README.md</code> for more information.</p>
-
-        <p>
-            <a href="https://vitejs.dev/guide/features.html" target="_blank"> Vite Docs </a>
-            |
-            <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-        </p>
-
-        <button type="button" @click="count++">count is: {{ count }}</button>
-        <p>
-            Edit
-            <code>components/HelloWorld.vue</code> to test hot module replacement.
-        </p>
-    </section>
-    <!-- Body end -->
-
-    <!-- Footer Start -->
-    <footer>
-        <nav>
-            <ul class="mobile-nav">
-                <li class="circle">L</li>
-                <li class="circle">C</li>
-                <li class="circle">A</li>
-            </ul>
-        </nav>
-    </footer>
-    <!-- Footer End -->
+  <!-- Footer Start -->
+  <footer>
+    <nav>
+      <ul class="mobile-nav">
+        <li class="circle">L</li>
+        <li class="circle">C</li>
+        <li class="circle">A</li>
+      </ul>
+    </nav>
+  </footer>
+  <!-- Footer End -->
 </template>
 
 <style lang="scss">
 @import "./assets/style.scss";
 
+//Header
+header {
+  position: absolute;
+  top: 5vh;
+  left: 0;
+
+  width: 100vw;
+  height: 10vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+// Search Menu
+
+.search-field {
+
+  display: grid;
+  grid-template-rows: repeat(2, 1fr);
+}
+
+.search-field > p {
+  margin-bottom: 0.25em;
+  padding: 0;
+  
+  color: white;
+
+  font-weight: 900;
+  font-size: 1.15em;
+
+  justify-self: start;
+  margin-left: 12vw;
+}
+
+.search-field > input {
+  width: 80vw;
+  height: 5vh;
+  
+  border: 0;
+  border-radius: 5px;
+
+  justify-self: center;
+  background-color: #437096;
+}
+
+.search-menu {
+  margin-top: 20vh;
+}
+
+.frosted-background {
+  border: 1px solid white;
+
+  height: 100vh
+}
+
+//Footer
+footer > nav > ul {
+  display: flex;
+  justify-content: space-evenly;
+
+  position: fixed;
+  bottom: 0;
+  background-color: #051522;
+
+  height: 3.5em;
+  width: 100vw;
+
+  padding: 1em 0rem;
+  margin: 0px;
+}
+
+//Defaults
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400&display=swap');
+
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #a9b5c1;
-    margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #a9b5c1;
+  margin-top: 60px;
+}
+
+body {
+  background-image: url("./assets/backsplash.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  // background-size: 100vw 80vh;
+
+  background-origin: border-box;
+  background-position-x: center;
+  background-position-y: center;
 }
 
 html,
 body {
-    margin: 0px;
-    padding: 0px;
+  margin: 0px;
+  padding: 0px;
 }
 
 h1 {
-  font-size: 1.30em;
+  font-size: 1.3em;
   color: white;
-  margin-left: 0.25em
+  margin-left: 0.25em;
 }
 
-body {
-    background-image: url("./assets/backsplash.jpg");
-    background-repeat: no-repeat;
-    background-size: cover;
-    // background-size: 100vw 80vh;
-    
-    background-origin: border-box;
-    background-position-x: center;
-    background-position-y: center;
-}
-
-header {
-    position: fixed;
-    top: 0.75em;
-    left: 0;
-    width: 100vw;
-    
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-
-footer > nav > ul {
-    display: flex;
-    justify-content: space-evenly;
-
-    position: fixed;
-    bottom: 0;
-    background-color: #051522;
-
-    height: 3.5em;
-    width: 100vw;
-
-    padding: 1em 0rem;
-    margin: 0px;
+h1, h2, h3, h4, h5, p {
+  font-family: 'Inter', sans-serif;
 }
 
 .circle {
-    display: flex;
+  display: flex;
 
-    border: 2px solid white;
-    width: 3rem;
-    height: 3rem;
-    border-radius: 25px;
+  border: 2px solid white;
+  width: 3rem;
+  height: 3rem;
+  border-radius: 25px;
 
-    align-items: center;
-    justify-content: center;
+  align-items: center;
+  justify-content: center;
 
-    user-select: none;
+  user-select: none;
 
-    &:hover {
-        cursor: pointer;
-        color: #42b983;
-    }
+  &:hover {
+    cursor: pointer;
+    color: #42b983;
+  }
 }
 
 .inline {
@@ -163,22 +192,22 @@ footer > nav > ul {
 }
 
 a {
-    color: #42b983;
+  color: #42b983;
 
-    &:hover {
-        color: black;
-    }
+  &:hover {
+    color: black;
+  }
 }
 
 label {
-    margin: 0 0.5em;
-    font-weight: bold;
+  margin: 0 0.5em;
+  font-weight: bold;
 }
 
 code {
-    background-color: #eee;
-    padding: 2px 4px;
-    border-radius: 4px;
-    color: #304455;
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
 }
 </style>
