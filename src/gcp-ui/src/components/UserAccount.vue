2992bdbd390c6ac96601d85edbc6012003abb2ef
Dawson K <46154365+TheInfinityIQ@users.noreply.github.com>
2022-04-14 15:21:41 -0500

    WIP account page done until bg change added

--- src/gcp-ui/src/components/UserAccount.vue
+++ src/gcp-ui/src/components/UserAccount.vue
@@ -1,47 +1,97 @@
-<script setup lang="ts">
-import { onMounted } from 'vue';
-import { BgType } from '../enums';
-
-const emit = defineEmits<{
-    (event: "bg-change", type?: BgType): void
-}>();
-
-onMounted(() => {
-    emit("bg-change", BgType.NoBackgrounds);
-    // emit("bg-change", BgType.NoBlurBackground);
-    // emit("bg-change", BgType.NoBackgroundPicture);
-})
+<script  setup lang="ts">
+import logo from './Logo.vue'
+import footerNav from './footer.vue'
 
 </script>
 
 <template>
     <form action="">
         <!-- UpdateAPI and return to home -->
-        <h1>User Information</h1>
-        <label for="username">UserName:</label>
-        <input type="text" id="username">
-        <label for="email">Email:</label>
-        <input type="email" id="email">
-        <label for="languages">Languages:</label>
-        <input type="text" id="languages">
+        <section class="user-information big-input">
+            <h1>User Information</h1>
+            <label for="username">UserName:</label>
+            <input type="text" id="username">
+            <label for="email">Email:</label>
+            <input type="email" id="email">
+            <label for="languages">Languages:</label>
+            <input type="text" id="languages">
+        </section>
         <!-- Inline -->
-        <div class="inline">
-            <label for="Pronouns"></label>
-            <input type="text" id="">
-            <label for="Age"></label>
-            <input type="number" id="">
-        </div>
+        <section class="inline">
+            <div class="small-input">
+                <label for="pronouns">Pronouns</label>
+                <input type="text" id="pronouns">
+            </div>
+            <div class="small-input">
+                <label for="age">Age</label>
+                <input type="number" id="age">
+            </div>
+        </section>
         <!-- End of inline -->
-        <h1>Linked Accounts</h1>
-        <label for="steam">Steam:</label>
-        <input type="text" id="steam">
-        <label for="discord">Discord</label>
-        <input type="text" id="discord">
-
-        <input type="submit" value="Apply">
+        <section class="linked-accounts big-input">
+            <h1>Linked Accounts</h1>
+            <label for="steam">Steam:</label>
+            <input type="text" id="steam">
+            <label for="discord">Discord</label>
+            <input type="text" id="discord">
+        </section>
+        <input type="submit" value="Apply" class="apply-button">
     </form>
 </template>
 
+<!-- <style>
+</style> -->
 
 <style scoped>
+body>#app {
+    background-image: none;
+    background-color: #051522;
+}
+
+input,
+label {
+    margin: 0;
+}
+
+input {
+    width: 95%;
+}
+
+.small-input > input {
+    width: 45%;
+}
+
+.small-input {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+form {
+    width: 100%;
+
+    overflow: scroll;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    padding: 0;
+}
+
+.inline {
+    display: inline-flex;
+}
+
+form > section {
+    width: 100%;
+}
+
+.apply-button {
+    width: 30%;
+    
+    min-width: 60px;
+}
+
+
 </style>
