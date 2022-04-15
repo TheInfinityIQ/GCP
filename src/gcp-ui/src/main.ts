c69c179e9187693ae1c191e9535e8e0fc8ab2cf9
Everett Grassler <45568848+Dragwar@users.noreply.github.com>
2022-04-15 17:05:20 -0500

    move router into router.ts

--- src/gcp-ui/src/main.ts
+++ src/gcp-ui/src/main.ts
@@ -1,36 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router';
 import { createApp } from 'vue';
+
 import 'vue-select/dist/vue-select.css';
 
-// Routes
 import App from './App.vue'
-import userGameList from './components/UserGameList.vue';
-import createGameList from './components/CreateGameList.vue';
-import searchGameList from './components/SearchGameList.vue';
-import helloWorld from './components/HelloWorld.vue'
-import userAccount from './components/UserAccount.vue'
-import SignIn from './components/SignIn.vue';
-import SignUp from './components/SignUp.vue';
+import { router } from './router';
 
-const routes = [
-  { path: '/search-games', component: searchGameList },
-  { path: '/user-games', component: userGameList },
-  { path: '/user-games/create', component: createGameList },
-  { path: '/hello-world', component: helloWorld },
-  { path: '/user-account', component: userAccount },
-  { path: '/sign-in', component: SignIn },
-  { path: '/sign-up', component: SignUp },
-  { path: '/', component: searchGameList }
-]
 
-const router = createRouter({
-  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
-  history: createWebHistory(),
-  routes, // short for `routes: routes`
-})
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
---
+++ src/gcp-ui/src/router.ts
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from "vue-router"
+
+import UserGameList from "./components/UserGameList.vue";
+import CreateGameList from "./components/CreateGameList.vue";
+import SearchGameList from "./components/SearchGameList.vue";
+import HelloWorld from "./components/HelloWorld.vue"
+import UserAccount from "./components/UserAccount.vue"
+import SignIn from "./components/SignIn.vue";
+import SignUp from "./components/SignUp.vue";
+
+const routes = [
+    { path: "/search-games", component: SearchGameList },
+    { path: "/user-games", component: UserGameList },
+    { path: "/user-games/create", component: CreateGameList },
+    { path: "/hello-world", component: HelloWorld },
+    { path: "/user-account", component: UserAccount },
+    { path: "/sign-in", component: SignIn },
+    { path: "/sign-up", component: SignUp },
+    { path: "/", component: SearchGameList }
+];
+
+const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHistory(),
+    routes, // short for `routes: routes`
+});
+
+export { router, routes };
